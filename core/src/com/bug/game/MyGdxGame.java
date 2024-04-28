7594d9e06a97687af06f67de71480ebfa9b26d0a
Ninjarunner26 <colins2605@protonmail.com>
2024-04-27 18:16:07 -0700

    Created fly sprite and music, loaded assets and played music

--- core/src/com/bug/game/MyGdxGame.java
+++ core/src/com/bug/game/MyGdxGame.java
@@ -4,19 +4,41 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture flyImage;
+	private Music bgMusic;
+	private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
-		return;
+		// Load images
+		flyImage = new Texture(Gdx.files.internal("fly.png"));
+
+		// Load sounds
+		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("bgm.mp3"));
+
+		// Play music
+		bgMusic.setLooping(true);
+		bgMusic.play();
+
+		// Init camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		//Init batch
+		batch = new SpriteBatch();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 1, 1);
+		ScreenUtils.clear(0, 1, 4, 1);
 	}
 	
 	@Override
